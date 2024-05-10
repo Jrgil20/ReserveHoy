20db258d3513c2320e72c2b132ebb76598b3dad3
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2024-05-10 05:48:58 -0400

    Refactor form styles and layout

--- assets/js/changeForm.js
+++ assets/js/changeForm.js
@@ -3,11 +3,20 @@ window.onload = function() {
         e.preventDefault();
         document.getElementById('restauranteForm').style.display = 'flex';
         document.getElementById('clienteForm').style.display = 'none';
+        document.querySelector('a.login').style.color = 'white';
+        document.querySelector('a.register').style.color = 'black';
+        document.querySelector('a.register').style.background= 'none';
+        document.querySelector('a.login').style.background = 'linear-gradient(90deg, #004225, #057c48)';
     });
 
     document.querySelector('.register').addEventListener('click', function(e) {
         e.preventDefault();
         document.getElementById('restauranteForm').style.display = 'none';
         document.getElementById('clienteForm').style.display = 'flex';
+        document.querySelector('a.login').style.color = 'black';
+        document.querySelector('a.register').style.color = 'white';
+        document.querySelector('a.login').style.background = 'none';
+        document.querySelector('a.register').style.background = 'linear-gradient(90deg, #004225, #057c48)';
+
     });
 }
