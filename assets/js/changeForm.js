a1378817a73cef0603a93c877a38ba8972a467b5
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2024-05-10 05:23:23 -0400

    Refactor form display logic in register.html and changeForm.js

--- assets/js/changeForm.js
+++ assets/js/changeForm.js
@@ -1,13 +1,13 @@
 window.onload = function() {
     document.querySelector('.login').addEventListener('click', function(e) {
         e.preventDefault();
-        document.getElementById('restauranteForm').style.display = 'block';
+        document.getElementById('restauranteForm').style.display = 'flex';
         document.getElementById('clienteForm').style.display = 'none';
     });
 
     document.querySelector('.register').addEventListener('click', function(e) {
         e.preventDefault();
         document.getElementById('restauranteForm').style.display = 'none';
-        document.getElementById('clienteForm').style.display = 'block';
+        document.getElementById('clienteForm').style.display = 'flex';
     });
 }
