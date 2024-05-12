17cc7ede689c6a578b7ceb3a84951be6c25158f1
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2024-05-11 22:59:35 -0400

    Refactor root route to use Spanish language in server.js

--- server.js
+++ server.js
@@ -67,8 +67,7 @@ app.post('/login', (req, res) => {
 // Define una ruta GET para la ruta raíz ("/"). Cuando alguien visita esta ruta, la función de devolución de llamada se ejecuta.
 // La función de devolución de llamada toma dos argumentos: un objeto de solicitud (que contiene información sobre la solicitud) y un objeto de respuesta (que se utiliza para enviar la respuesta).
 app.get('/', (req, res) => {
-  // Envía la respuesta "Hola Mundo!".
-  res.send('Servidor de prueba JR');
+  res.send('Esta es la raiz de la aplicación.');
 });
 
 // Inicia el servidor en el puerto especificado. Una vez que el servidor está en marcha, se ejecuta la función de devolución de llamada.
