5d53b747ef833992f1d3a279456e1feecce00f2e
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2024-05-13 02:16:04 -0400

    chore: Update changeForm.js to use querySelector instead of getElementById

--- public/assets/js/changeForm.js
+++ public/assets/js/changeForm.js
@@ -1,43 +1,22 @@
 window.onload = function() {
-    document.querySelector('.restauranteRegister').addEventListener('click', function(e) {
+    document.querySelector('restaurante-btn').addEventListener('click', function(e) {
         e.preventDefault();
-        document.getElementById('restauranteRegisterForm').style.display = 'flex';
-        document.getElementById('clienteRegisterForm').style.display = 'none';
+        document.querySelector('restauranteForm').style.display = 'flex';
+        document.querySelector('clienteRegisterForm').style.display = 'none';
         document.querySelector('a.restauranteRegister').style.color = 'white';
         document.querySelector('a.clienteRegister').style.color = 'black';
         document.querySelector('a.clienteRegister').style.background= 'none';
         document.querySelector('a.restauranteRegister').style.background = 'linear-gradient(90deg, #004225, #057c48)';
     });
 
-    document.querySelector('.clienteRegister').addEventListener('click', function(e) {
+    document.querySelector('cliente-btn').addEventListener('click', function(e) {
         e.preventDefault();
-        document.getElementById('restauranteRegisterForm').style.display = 'none';
-        document.getElementById('clienteRegisterForm').style.display = 'flex';
+        document.querySelector('restauranteForm').style.display = 'none';
+        document.querySelector('clienteRegisterForm').style.display = 'flex';
         document.querySelector('a.restauranteRegister').style.color = 'black';
         document.querySelector('a.clienteRegister').style.color = 'white';
         document.querySelector('a.restauranteRegister').style.background = 'none';
         document.querySelector('a.clienteRegister').style.background = 'linear-gradient(90deg, #004225, #057c48)';
-
-    });
-
-    document.querySelector('.restauranteLogin').addEventListener('click', function(e) {
-        e.preventDefault();
-        document.getElementById('restauranteLoginForm').style.display = 'flex';
-        document.getElementById('clienteLoginForm').style.display = 'none';
-        document.querySelector('a.restauranteLogin').style.color = 'white';
-        document.querySelector('a.clienteLogin').style.color = 'black';
-        document.querySelector('a.restauranteLogin').style.background= 'none';
-        document.querySelector('a.clienteLogin').style.background = 'linear-gradient(90deg, #004225, #057c48)';
-    });
-
-    document.querySelector('.clienteLogin').addEventListener('click', function(e) {
-        e.preventDefault();
-        document.getElementById('restauranteLoginForm').style.display = 'flex';
-        document.getElementById('clienteLoginForm').style.display = 'none';
-        document.querySelector('a.restauranteLogin').style.color = 'black';
-        document.querySelector('a.clienteLogin').style.color = 'white';
-        document.querySelector('a.restauranteLogin').style.background = 'none';
-        document.querySelector('a.clienteLogin').style.background = 'linear-gradient(90deg, #004225, #057c48)';
 
     });
 }
