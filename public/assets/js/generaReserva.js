44106c7c92e7f17caa62eae309e4d27278f7da1d
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2024-05-14 17:28:35 -0400

    chore: Refactor buscaReserva function to use async/await syntax

--- public/assets/js/generaReserva.js
+++ public/assets/js/generaReserva.js
@@ -1,15 +1,18 @@
-let idDeLaReserva = '123'; // este id debe ser el id de la reserva que quieres buscar
 
-fetch('/buscarReserva/' + idDeLaReserva)
-.then(response => response.json())
-.then(data => {
+async function buscarReserva(idDeLaReserva) {
+  try {
+    const response = await fetch('/buscarReserva/' + idDeLaReserva);
+    const data = await response.json();
+
     console.log(data);
     document.getElementById('NumeroDeReserva').textContent = data.idReserva;
     document.getElementById('NPersonas').textContent = data.numeroPersona;
     document.getElementById('FechaQueSeReserva').textContent = data.fecha;
     document.getElementById('HoraDeReserva').textContent = data.hora;
     document.getElementById('CorreoDeQuienReserva').textContent = data.correoCli;
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+buscarReserva('123'); // este id debe ser el id de la reserva que quieres buscar
