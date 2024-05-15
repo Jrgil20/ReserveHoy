828d8f0052c18140839491dbd1f0ace46c1393e2
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2024-05-15 02:44:49 -0400

    chore: Update buscaReserva function to use async/await syntax and improve console logging

--- public/assets/js/generaReserva.js
+++ public/assets/js/generaReserva.js
@@ -9,14 +9,20 @@ buscarReserva(idDeLaReserva);
 async function buscarReserva(idDeLaReserva) {
   try {
     const response = await fetch('/buscarReserva/' + idDeLaReserva);
+    //hacemos una peticion a la ruta /buscarReserva/ + el valor de la url
+    //la palabra reservada await hace que la funcion se espere a que la promesa se resuelva
     const data = await response.json();
+    //esperamos a que la promesa se resuelva y guardamos el resultado en la variable data
+    //la funcion json() convierte la respuesta del servidor en un objeto json
 
-    console.log(data);
+    console.log('Datos de respuesta:',data);
+    // se imprime en consola el objeto data para comprobar que se obtuvo la informacion
     document.getElementById('NumeroDeReserva').textContent = data.idReserva;
     document.getElementById('NPersonas').textContent = data.numeroPersona;
     document.getElementById('FechaQueSeReserva').textContent = data.fecha;
     document.getElementById('HoraDeReserva').textContent = data.hora;
     document.getElementById('CorreoDeQuienReserva').textContent = data.correoCli;
+    // se actualizza los elementos del html con la informacion obtenida del servidor
   } catch (error) {
     console.error('Error:', error);
   }
