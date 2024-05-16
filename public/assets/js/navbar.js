84faa935a26374c9509e97ed949af02c2371e43d
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2024-05-16 07:50:20 -0400

    chore: Update navigation links in historialReservas.html and perfil.html

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -3,6 +3,11 @@ var correoRestaurante = sessionStorage.getItem('correoRestaurante');
 
 if (correoRestaurante) {
   document.getElementById('login-button-nav').href = 'perfil.html?restaurante=' + correoRestaurante;
+  document.getElementById('ConsultaLink').href = 'historialReservas.html?restaurante=' + correoRestaurante;
 } else {
   document.getElementById('login-button-nav').href = 'login.html';
+  document.getElementById('ConsultaLink').href = 'ConsultarReserva.html';
 }
+
+
+ 
