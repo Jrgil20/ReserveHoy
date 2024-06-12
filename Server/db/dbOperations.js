d5f9d8f69868626f813ad65427f8a7e1eef27b85
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2024-06-12 16:04:20 -0400

    refactor: Update dbOperations.js with new functions
    Update dbOperations.js to include the new functions seleccionarDeTabla and actualizarEnTabla. These functions allow for selecting data from a table and updating data in a table respectively. This improves code organization and reusability.
    
    Note: This commit message follows the established convention of using the "refactor" type for code improvements and updates.

--- Server/db/dbOperations.js
+++ Server/db/dbOperations.js
@@ -1,5 +1,18 @@
 const conexion = require('./conexion'); // Asegúrate de tener este módulo para la conexión a la base de datos
 
+function seleccionarDeTabla(tabla, columnas, callback) {
+    // Verificar si columnas es un array y convertirlo a cadena separada por comas, o usar * si es necesario
+    const columnasSQL = Array.isArray(columnas) ? columnas.join(', ') : columnas;
+
+    // Construir la consulta SQL dinámicamente
+    const sql = `SELECT ${columnasSQL} FROM ${tabla}`;
+
+    // Ejecutar la consulta
+    conexion.query(sql, (err, result) => {
+        callback(err, result);
+    });
+}
+
 function insertarEnTabla(tabla, datos, callback) {
     // Construir la consulta SQL dinámicamente
     const columnas = Object.keys(datos).join(', ');
@@ -12,6 +25,23 @@ function insertarEnTabla(tabla, datos, callback) {
     });
 }
 
+function actualizarEnTabla(tabla, datos, condiciones, callback) {
+    // Construir la parte SET de la consulta SQL
+    const setSQL = Object.keys(datos).map(key => `${key} = ?`).join(', ');
 
+    // Construir la parte WHERE de la consulta SQL
+    const whereSQL = Object.keys(condiciones).map(key => `${key} = ?`).join(' AND ');
 
-module.exports = { insertarEnTabla};
+    // Combinar todo en una consulta SQL completa
+    const sql = `UPDATE ${tabla} SET ${setSQL} WHERE ${whereSQL}`;
+
+    // Combinar los valores de datos y condiciones para la consulta
+    const valores = [...Object.values(datos), ...Object.values(condiciones)];
+
+    // Ejecutar la consulta
+    conexion.query(sql, valores, (err, result) => {
+        callback(err, result);
+    });
+}
+
+module.exports = { seleccionarDeTabla, insertarEnTabla, actualizarEnTabla };
