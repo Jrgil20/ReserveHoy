161e335105b06eec913d46425f0ea5c9a03aecc8
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2024-07-03 06:16:52 -0400

    feat: Add dynamic display of restaurant dishes in perfil.html

--- public/assets/js/buscarPlato.js
+++ public/assets/js/buscarPlato.js
@@ -4,8 +4,34 @@ const correoResPlat = sessionStorage.getItem('correoRestaurante');
 fetch(`/consultarPlatos/${correoResPlat}`)
     .then(response => response.json())
     .then(data => {
-        // Aquí puedes trabajar con la lista de platos obtenida en 'data'
         console.log(data);
+    
+        let contenedorPlatos = document.getElementById('platos'); // Este es el contenedor principal
+        let platosPorTipo = {};
+    
+        data.forEach(plato => {
+            let tipo = plato.tipo;
+            if (!platosPorTipo[tipo]) {
+                platosPorTipo[tipo] = [];
+            }
+            platosPorTipo[tipo].push(plato);
+        });
+    
+        for (let tipo in platosPorTipo) {
+            let tipoPlato = document.createElement('li');
+            tipoPlato.textContent = tipo;
+    
+            let listaPlatos = document.createElement('ul'); // Cambio de nombre para evitar confusión
+            platosPorTipo[tipo].forEach(plato => {
+                let platoItem = document.createElement('li');
+                platoItem.textContent = plato.nombrePlato;
+                listaPlatos.appendChild(platoItem); // Agregar platoItem a listaPlatos, no a platos
+            });
+    
+            tipoPlato.appendChild(listaPlatos); // Agregar listaPlatos a tipoPlato
+    
+            contenedorPlatos.appendChild(tipoPlato); // Agregar tipoPlato a contenedorPlatos, no a sí mismo
+        }
     })
     .catch(error => {
         console.error('Error:', error);
